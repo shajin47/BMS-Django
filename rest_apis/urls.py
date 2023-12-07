e431beed2170a4ff69b1c650a84fa395bcab1b27
Kabhilan05 <kabhilan05@gmail.com>
2023-12-07 18:19:55 +0530

    getuser with protected route completed

--- rest_apis/urls.py
+++ rest_apis/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name="add_new_user"),
     path('register/admin/', views.admin_register, name="add_new_admin"),
-
     path('login/', views.user_login, name="login"),
     path('logout/', views.user_logout, name="logout"),
+    path('getuser/<pk>/', views.get_user, name="getuser"),
 ]
