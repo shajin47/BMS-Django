3454de0220835a0526bc337e3e57267080d0edf2
Kabhilan05 <kabhilan05@gmail.com>
2023-12-07 17:37:11 +0530

    Group created ,and admin url created

--- rest_apis/urls.py
+++ rest_apis/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name="add_new_user"),
+    path('register/admin/', views.admin_register, name="add_new_admin"),
+
     path('login/', views.user_login, name="login"),
     path('logout/', views.user_logout, name="logout"),
 ]
