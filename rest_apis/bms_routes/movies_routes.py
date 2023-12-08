9a2f27a8cf967153a042bc6f4b3754139251d23f
Kabhilan05 <kabhilan05@gmail.com>
2023-12-08 20:40:18 +0530

    Proteted route created for Create_movie

--- rest_apis/bms_routes/movies_routes.py
+++ rest_apis/bms_routes/movies_routes.py
@@ -1,22 +1,33 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,authentication_classes, permission_classes
 from rest_framework.response import Response
 from ..models import Movies
 from datetime import timedelta
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 @api_view(["POST"])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
 def create_movie(request):
     try:
-        movie = Movies.objects.create( movie_name= request.data.get("movie_name"),
-            release_date = request.data.get("release_date"),
-            duration= timedelta(days=request.data.get("duration").get('days'),
-                                hours=request.data.get("duration").get('hours'),
-                                minutes=request.data.get("duration").get('minutes')),
-            rating= request.data.get("rating"),
-            language = request.data.get("language"),
-            genre = request.data.get("genre"),
-            cbc = request.data.get("cbc"),
-            movie_description= request.data.get("movie_description"))
+        user = request.user
+        if user.groups.filter(name = 'Admin Group').exists():
+            movie = Movies.objects.create( movie_name= request.data.get("movie_name"),
+                release_date = request.data.get("release_date"),
+                duration= timedelta(days=request.data.get("duration").get('days'),
+                                    hours=request.data.get("duration").get('hours'),
+                                    minutes=request.data.get("duration").get('minutes')),
+                rating= request.data.get("rating"),
+                language = request.data.get("language"),
+                genre = request.data.get("genre"),
+                cbc = request.data.get("cbc"),
+                movie_description= request.data.get("movie_description"))
 
-        movie.save()
-        return Response("Success")
+            movie.save()
+            return Response({"Success": True,
+                             "message":"New Movie has been added"})
+        return Response({"Success": False,
+                             "message":"You do not have Permission"})
     except Exception as e:
         return Response(f"There is the error :{e}")
+
