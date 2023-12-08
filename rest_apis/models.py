d595da0b889c5a38019098f9909f9df885a01a21
Kabhilan05 <kabhilan05@gmail.com>
2023-12-08 20:06:01 +0530

    Views for create_movie created

--- rest_apis/models.py
+++ rest_apis/models.py
@@ -10,12 +10,13 @@ class CustomUser(AbstractUser):
 
 class Movies(models.Model):
     movie_name = models.CharField(max_length=256,blank=False)
-    release_date = models.DateField(blank=False)
-    duration = models.DurationField(blank=False)
-    rating = models.DecimalField(decimal_places=2,max_digits=2,default=0.0,validators=[MaxValueValidator(limit_value=10)])
+    # movie_poster = models.FieldFile(upload_to ="..media/" )
+    release_date = models.DateField()
+    duration = models.DurationField()
+    rating = models.DecimalField(decimal_places=2,max_digits=4,default=0.0,validators=[MaxValueValidator(limit_value=10)])
     language = models.CharField(max_length=256)
-    genre=models.CharField(max_length=256,blank=False)
-    cbc = models.CharField(max_length=256,blank=False)
+    genre=models.CharField(max_length=256)
+    cbc = models.CharField(max_length=256)
     movie_description  = models.TextField(max_length=2000)
     created_time = models.DateTimeField(auto_now=True)
     modified_time = models.DateTimeField(auto_now_add=True)
